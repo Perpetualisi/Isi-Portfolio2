1b5b53cd02db49ff5a7b007e74c5b5c67df8b4ed
Isi Name <perpetualokan5@gmail.com>
2025-04-03 22:27:26 +0100

    Updated Mywork component for automatic slide effect and smooth scroll

--- src/Components/Mywork/Mywork.jsx
+++ src/Components/Mywork/Mywork.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
-import './Mywork.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import mywork_data from '../../assets/mywork_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import React, { useState, useEffect } from 'react';
+import './Mywork.css';
+import theme_pattern from '../../assets/theme_pattern.svg';
+import mywork_data from '../../assets/mywork_data';
+import arrow_icon from '../../assets/arrow_icon.svg';
 
 const Mywork = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === mywork_data.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change slide every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div id='work' className='mywork'>
+    <div id="work" className="mywork">
       <div className="mywork-title">
         <h1>My latest work</h1>
         <img src={theme_pattern} alt="Theme Pattern" />
       </div>
-      
+
       <div className="mywork-container">
         {mywork_data.map((work, index) => (
-          <img 
-            key={index} 
-            src={work.w_img} 
-            alt={`Project ${index + 1}`} 
+          <img
+            key={index}
+            src={work.w_img}
+            alt={`Project ${index + 1}`}
+            className={`work-slide ${index === currentIndex ? 'active' : ''}`}
           />
         ))}
       </div>
-      
-      <div className='mywork-showmore'>
-        <p>Show More</p>
+
+      <div className="mywork-showmore">
+        {/* Link to About section */}
+        <a href="#services" className="anchor-link">
+          <p>Show More</p>
+        </a>
         <img src={arrow_icon} alt="Arrow" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mywork
+export default Mywork;
