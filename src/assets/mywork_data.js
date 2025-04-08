cbf2856f5959beba97c6d4aa368a90c491a8ce11
Isi Name <perpetualokan5@gmail.com>
2025-04-08 14:54:27 +0100

    Updated hotel.jpg and mywork_data.js; added new image assets

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,8 +1,8 @@
 import dental from '../assets/dental.jpg'
-import university from '../assets/university.jpg'
-import movie from '../assets/movie.jpg'
-import cv from '../assets/cv.jpg'
-import hotel from '../assets/hotel.jpg'
+import pamsb from '../assets/pamsb.jpg'
+import tasty from '../assets/tasty.jpg'
+import edusity from '../assets/edusity.png'
+import hotel from '../assets/hotel.jpg' // Ensure the correct path for the image
 import cook from '../assets/cook.jpg'
 
 const mywork_data = [
@@ -14,17 +14,17 @@ const mywork_data = [
     {
         w_no:2,
         w_name:"Web design",
-        w_img:university
+        w_img:pamsb
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:movie
+        w_img:tasty
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:cv
+        w_img:edusity
     },
     {
         w_no:5,
