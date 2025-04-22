30c0196190e21d063619b72931fbab69c6a8381c
Isi Name <perpetualokan5@gmail.com>
2025-04-22 10:16:37 +0100

    Update About section with new skills

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -17,34 +17,45 @@ const About = () => {
         <div className="about-right">
           <div className="about-para">
             <p>
-            Hey there! I’m Okan Perpetual Isi, a frontend web developer with 2 years of experience. I love building clean, responsive, and user-friendly websites. I work with HTML, CSS, JavaScript, and React to bring ideas to life on the web.
-
-
-{" "}
+              Hey there! I’m Okan Perpetual Isi, a frontend web developer with 2 years of experience. I love building clean, responsive, and user-friendly websites. I work with HTML, CSS, JavaScript, and React to bring ideas to life on the web.
             </p>
             <p>
-            I also enjoy learning new things and improving my skills every day. Whether it’s a landing page, a business site, or something creative, I’m here to make it happen.
-
-Let’s build something amazing together! 
-{" "}
+              I also enjoy learning new things and improving my skills every day. Whether it’s a landing page, a business site, or something creative, I’m here to make it happen.
+              Let’s build something amazing together!
             </p>
           </div>
           <div className="about-skills">
             <div className="about-skill">
               <p>HTML & CSS</p>
-              <hr style={{ width: "50%" }} />
+              <hr style={{ width: "100%" }} />
             </div>
             <div className="about-skill">
               <p>React Js</p>
-              <hr style={{ width: "70%" }} />
+              <hr style={{ width: "90%" }} />
             </div>
             <div className="about-skill">
               <p>JavaScript</p>
+              <hr style={{ width: "80%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Next Js</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Tailwind CSS</p>
               <hr style={{ width: "60%" }} />
             </div>
             <div className="about-skill">
-              <p>Next Js </p>
-              <hr style={{ width: "50%" }} />
+              <p>WordPress</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Vite</p>
+              <hr style={{ width: "75%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Git & GitHub</p>
+              <hr style={{ width: "85%" }} />
             </div>
           </div>
         </div>
