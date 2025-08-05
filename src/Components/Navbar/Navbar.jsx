288b9720ea2c4681c413cc327d447b49eb1a3742
Isi Name <perpetualokan5@gmail.com>
2025-08-05 13:54:37 +0100

    Update Navbar styles and functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-import okan from '../../assets/okan.png';
 import underline from '../../assets/nav_underline.svg';
 import menu_open from '../../assets/menu_open.svg';
 import menu_close from '../../assets/menu_close.svg';
@@ -21,7 +20,7 @@ const Navbar = () => {
 
   const handleLinkClick = (section) => {
     setMenu(section);
-    closeMenu(); 
+    closeMenu();
   };
 
   useEffect(() => {
@@ -35,47 +34,54 @@ const Navbar = () => {
 
   return (
     <div className={`navbar ${isScrolled ? 'navbar-scroll' : ''}`}>
-      <img src={okan} alt="Logo" className='logo' /> 
+      <div className="logo-text">
+        <Link to="/" onClick={() => handleLinkClick("home")}>
+          OKAN
+        </Link>
+      </div>
+
       <img src={menu_open} onClick={openMenu} alt="Open Menu" className='nav-mob-open' />
+
       <ul ref={menuRef} className='nav-menu'>
         <img src={menu_close} onClick={closeMenu} alt="Close Menu" className="nav-mob-close" />
         <li>
           <Link className='anchor-link' to="/" onClick={() => handleLinkClick("home")}>
             Home
           </Link>
-          {menu === "home" ? <img src={underline} alt='' /> : null}
+          {menu === "home" && <img src={underline} alt='' />}
         </li>
         <li>
           <Link className='anchor-link' to="/about" onClick={() => handleLinkClick("about")}>
             About me
           </Link>
-          {menu === "about" ? <img src={underline} alt='' /> : null}
+          {menu === "about" && <img src={underline} alt='' />}
         </li>
         <li>
           <Link className='anchor-link' to="/portfolio" onClick={() => handleLinkClick("portfolio")}>
             Portfolio
           </Link>
-          {menu === "portfolio" ? <img src={underline} alt='' /> : null}
+          {menu === "portfolio" && <img src={underline} alt='' />}
         </li>
         <li>
           <Link className='anchor-link' to="/services" onClick={() => handleLinkClick("services")}>
             Services
           </Link>
-          {menu === "services" ? <img src={underline} alt='' /> : null}
+          {menu === "services" && <img src={underline} alt='' />}
         </li>
         <li>
           <Link className='anchor-link' to="/work" onClick={() => handleLinkClick("work")}>
             Work
           </Link>
-          {menu === "work" ? <img src={underline} alt='' /> : null}
+          {menu === "work" && <img src={underline} alt='' />}
         </li>
         <li>
           <Link className='anchor-link' to="/contact" onClick={() => handleLinkClick("contact")}>
             Contact
           </Link>
-          {menu === "contact" ? <img src={underline} alt='' /> : null}
+          {menu === "contact" && <img src={underline} alt='' />}
         </li>
       </ul>
+
       <div className='nav-connect'>
         <Link className='anchor-link' to="/contact" onClick={() => handleLinkClick("contact")}>
           Connect With Me
@@ -86,13 +92,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
-
-
-
-
-
-
-
